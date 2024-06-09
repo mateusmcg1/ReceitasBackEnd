80e25e0501640806707c431ea093dba5be9bdbd4
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2024-06-08 21:33:09 -0300

    recipe done

--- Routes/ChefRoute.js
+++ Routes/ChefRoute.js
@@ -11,7 +11,7 @@ router.post("/adminlogin", (req, res) => {
     FROM Funcionario f 
     JOIN Cargo c ON f.idCargo = c.idCargo 
     WHERE f.email = ? AND f.password = ?`;
-  
+
   con.query(sql, [req.body.email, req.body.password], (err, result) => {
     if (err) return res.json({ loginStatus: false, Error: "Query error" });
     if (result.length > 0) {
@@ -22,18 +22,20 @@ router.post("/adminlogin", (req, res) => {
         { expiresIn: "1d" }
       );
       res.cookie("token", token);
-      return res.json({ loginStatus: true, idToken: { jwtToken: token, refreshToken: "dummy_refresh_token" } });
+      return res.json({
+        loginStatus: true,
+        idToken: { jwtToken: token, refreshToken: "dummy_refresh_token" },
+      });
     } else {
       return res.json({ loginStatus: false, Error: "wrong email or password" });
     }
   });
 });
 
-
 router.post("/add_categoria", (req, res) => {
   const sql = "INSERT INTO categoria (`nome`) VALUES (?)";
   con.query(sql, [req.body.nome], (err, result) => {
-    if (err) return res.json({ Status: false, Error: "Query Error"+err });
+    if (err) return res.json({ Status: false, Error: "Query Error" + err });
     return res.json({ Status: true });
   });
 });
@@ -67,10 +69,7 @@ router.get("/edit_categoria/:id", (req, res) => {
 router.put("/edit_categoria/:id", (req, res) => {
   const id = req.params.id;
   const sql = `UPDATE categoria SET nome=? WHERE idCategoria=?`;
-  const values = [
-    req.body.nome,
-    id,
-  ];
+  const values = [req.body.nome, id];
   con.query(sql, values, (err, result) => {
     if (err) return res.json({ Status: false, Error: "Query Error" + err });
     return res.json({ Status: true, Result: result });
@@ -81,24 +80,24 @@ router.post("/add_receita", (req, res) => {
   const sql = `INSERT INTO receita 
     (nome,Data,ModoPreparo, QuantidadePorcao, IndRecInedita, idCozinheiro, Categoria) 
     VALUES (?)`;
-    const values = [
-      req.body.nome,
-      req.body.Data,
-      req.body.ModoPreparo,
-      req.body.QuantidadePorcao,
-      req.body.IndRecInedita,
-      req.body.idCozinheiro,
-      req.body.idCategoria,
-    ];
-    con.query(sql, [values], (err, result) => {
-      if (err) return res.json({ Status: false, Error: "Query Error con" +err });
-      return res.json({ Status: true });
-    });
+  const values = [
+    req.body.nome,
+    req.body.Data,
+    req.body.ModoPreparo,
+    req.body.QuantidadePorcao,
+    req.body.IndRecInedita,
+    req.body.idCozinheiro,
+    req.body.idCategoria,
+  ];
+  con.query(sql, [values], (err, result) => {
+    if (err) return res.json({ Status: false, Error: "Query Error con" + err });
+    return res.json({ Status: true });
+  });
 });
 
 router.get("/receita", (req, res) => {
   const sql = `
-    SELECT r.*, f.Nome AS NomeFuncionario, c.Nome AS NomeCategoria 
+    SELECT r.*, f.Nome AS Cozinheiro, c.Nome AS Categoria 
     FROM receita r 
     JOIN funcionario f ON r.idCozinheiro = f.idFuncionario 
     JOIN categoria c ON r.Categoria = c.idCategoria`;
@@ -109,6 +108,46 @@ router.get("/receita", (req, res) => {
   });
 });
 
+router.delete("/delete_receita/:nome/:idCozinheiro", (req, res) => {
+  const nome = req.params.nome;
+  const idCozinheiro = req.params.idCozinheiro;
+  const sql = "DELETE from receita WHERE nome=? AND idCozinheiro=?";
+  const values = [nome, idCozinheiro];
+  con.query(sql, values, (err, result) => {
+    if (err) return res.json({ Status: false, Error: "Query Error: " + err });
+    return res.json({ Status: true, Result: result });
+  });
+});
 
+router.get("/receita/:nome/:idCozinheiro", (req, res) => {
+  const nome = req.params.nome;
+  const idCozinheiro = req.params.idCozinheiro;
+  const sql = "SELECT * FROM receita WHERE nome=? AND idCozinheiro=?";
+  con.query(sql, [nome, idCozinheiro], (err, result) => {
+    if (err) return res.json({ Status: false, Error: "Query Error" + err });
+    return res.json({ Status: true, Result: result });
+  });
+});
+
+router.put("/edit_receita/:nome/:idCozinheiro", (req, res) => {
+  const nome = req.params.nome;
+  const idCozinheiro = req.params.idCozinheiro;
+  const sql = `UPDATE receita SET nome=?, Data=?, IndRecInedita=?, idCozinheiro=?, Categoria=?, QuantidadePorcao=?, ModoPreparo=?  WHERE nome=? AND idCozinheiro=?`;
+  const values = [
+    req.body.nome,
+    req.body.Data,
+    req.body.IndRecInedita,
+    req.body.idCozinheiro,
+    req.body.Categoria,
+    req.body.QuantidadePorcao,
+    req.body.ModoPreparo,
+    nome,
+    idCozinheiro
+  ];
+  con.query(sql, values, (err, result) => {
+    if (err) return res.json({ Status: false, Error: "Query Error" + err });
+    return res.json({ Status: true, Result: result });
+  });
+});
 
 export { router as chefRouter };
